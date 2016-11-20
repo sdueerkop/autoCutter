a63e976ba0fd23740ce6e915070d78341dc256de
Sven Düerkop <sdueerkop@uni-bremen.de>
2016-11-20 12:12:26 +0100

    Ordner können durchlaufen werden, xlsx-Dateien werden gefunden, output-Dateiname definiert

--- readXlsx.py
+++ readXlsx.py
@@ -1,20 +1,11 @@
 #! /usr/bin/env python3
 
-import os
+import os, glob
 import openpyxl
 import logging
 
 # Logging configuration
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-
-#TODO: Get root dir of the project
-#TODO: Get list of folders
-#TODO: For each folder chdir into the folder
-
-#TODO: Create a 'parts' folder
-
-#TODO: Extract the folder specific name (to be used in the output files of ffmpeg)
-
 
 def readFromXlsx(filename):
     wb = openpyxl.load_workbook(filename, read_only=True)
@@ -23,13 +14,38 @@ def readFromXlsx(filename):
         tempList = []
         for cellObj in rowOfCellObjects:
             tempList.append(cellObj.value)
-
-        print(tuple(tempList))
+        logging.debug(tuple(tempList))
 
 #TODO: Cut the video with ffmpeg
 
 def main():
-    readFromXlsx('testWorkbook.xlsx')
+    # Get root dir of the project
+    src_dir = input("Please enter the path to your video directories: ")
+    logging.debug("Source dir set to: {}".format(src_dir))
+
+    # Get list of folders
+    list_of_dirs = next(os.walk(src_dir))[1]
+    logging.debug(list_of_dirs)
+
+    # For each folder chdir into the folder
+    for d in list_of_dirs:
+        os.chdir(os.path.join(src_dir,d))
+        logging.debug("Current path is: {}".format(os.getcwd()))
+
+        # Create a 'parts' folder
+        try:
+            os.mkdir('parts')
+        except FileExistsError:
+            logging.debug("Folder 'parts' exists in {}".format(os.getcwd())) 
+
+        #Find xlsx files
+        xlsx = [f for f in glob.glob("*.xlsx") and glob.glob("Timestamp_*")]
+        logging.debug("In dir {} is {}".format(os.getcwd(), xlsx[0]))
+
+        #: Extract the folder specific name (to be used in the output files of ffmpeg)
+        output_name = xlsx[0].strip(".xlsx")
+        output_name = output_name.strip("Timestamp_")
+        logging.debug("Output name is: {}".format(output_name))
 
 if __name__ == "__main__":
     main()
